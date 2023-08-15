9c8e92010d29375923e47a1173d7364a5fbb727b
iliatarasov <129177692+iliatarasov@users.noreply.github.com>
2023-08-15 20:14:52 +0600

    add random forest

---
+++ my_models/random_forest.py
@@ -0,0 +1,41 @@
+from typing import Any
+from collections import Counter
+import numpy as np
+
+from .desicion_tree import MyDTClassifier
+
+
+class MyRFClassifier:
+    def __init__(self, n_estimators: int=50):
+        self.n_estimators = n_estimators
+    
+    def test_accuracy(self, tree: MyDTClassifier, test_data: np.ndarray) -> float:
+        predictions = tree.predict(test_data[:, :-1])
+        return sum(predictions == test_data[:, -1]) / len(predictions)
+        
+    def bootstrap(self, data: np.ndarray) -> tuple[np.ndarray]:
+        choice = np.random.choice(range(len(data)), len(data), 
+                                        replace = True)
+        bootstrap_idx = np.zeros(data.shape[0], dtype=bool)
+        bootstrap_idx[choice] = True
+        test_idx = ~bootstrap_idx
+        return data[bootstrap_idx], data[test_idx]
+    
+    def fit(self, data: np.ndarray) -> None:
+        self.trees = []
+        self.test_scores = []
+        for _ in range(self.n_estimators):
+            bootstrap, test = self.bootstrap(data)
+            tree = MyDTClassifier()
+            tree.fit(bootstrap)
+            self.trees.append(tree)
+            test_score = self.test_accuracy(tree, test)
+            self.test_scores.append(test_score)
+        
+    def predict(self, data: np.ndarray) -> np.ndarray[Any]:
+        predictions = []
+        for row in data:
+            ensemble_predictions = [tree.predict(row) for tree in self.trees]
+            counter = Counter(ensemble_predictions)
+            predictions.append(counter.most_common(1)[0][0])
+        return np.array(predictions)
