e8680d2573f36fa03141e9399a247c1175a1f036
iliatarasov <129177692+iliatarasov@users.noreply.github.com>
2023-08-16 19:01:34 +0600

    add docstrings and typing

--- my_models/desicion_tree.py
+++ my_models/desicion_tree.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas
 
 class Question:
+    '''Question support class for desicion building'''
     def __init__(self, column: int, value: int|float) -> None:
         self.column = column
         self.value = value
@@ -23,25 +24,30 @@ class Question:
             return value == self.value
 
 class Leaf:
+    '''Leaf node class for desicion tree'''
     def __init__(self, data: np.ndarray) -> None:
         self.predictions = collections.Counter(data[:, -1])
         
     def predict(self) -> Any:
+        '''Returns a random value from the dictionary of labels in the leaf.
+        The probability of each item being chosen is proportional to the number
+        of instances of that item in the leaf'''
         s = sum(self.predictions.values())
         p = [value / s for value in self.predictions.values()]
         return random.choices(list(self.predictions.keys()), p)[0]
         
 class DesicionNode:
+    '''Desicion node class for desicion tree'''
     def __init__(self, question: Question, true_branch: Self|Leaf,
                  false_branch: Self|Leaf) -> None:
+        '''Each desicion node is described by the question that it asks and
+        two children: one for True answer and one for False'''
         self.question = question
         self.true_branch = true_branch
         self.false_branch = false_branch
         
 class MyDTClassifier:
-    def __init__(self):
-        pass
-    
+    '''Desicion tree classifier class'''
     def partition(self, data: np.ndarray, question: Question) -> tuple[list]:
         '''Partitions a dataset by answer to the question'''
         true_rows, false_rows = [], []
@@ -68,6 +74,7 @@ class MyDTClassifier:
         return ig
     
     def find_best_split(self, data: np.ndarray) -> tuple[float, Question]:
+        '''Finds the best split on a given data subset'''
         best_gain = 0
         best_question = None
         cur_uncertainty = self.gini(data)
@@ -85,6 +92,11 @@ class MyDTClassifier:
         return best_gain, best_question
     
     def build_tree(self, data: np.ndarray) -> DesicionNode|Leaf:
+        '''
+        Constructs the tree
+        Returns:
+            tree head node (DesicionNode)
+        '''
         gain, question = self.find_best_split(data)
         if gain == 0:
             return Leaf(data)
@@ -94,6 +106,10 @@ class MyDTClassifier:
         return DesicionNode(question, true_branch, false_branch)
     
     def classify(self, row: np.ndarray, node: DesicionNode|Leaf) -> None:
+        '''
+        Returns a label for a given datapoint predicted by a branch growing 
+        from node
+        '''
         if isinstance(node, Leaf):
             return node.predict()
         if node.question.match(row):
@@ -102,11 +118,15 @@ class MyDTClassifier:
             return self.classify(row, node.false_branch)
             
     def fit(self, data: np.ndarray) -> None:
+        '''
+        Training procedure. Calls build_tree and saves the head of the tree
+        '''
         if isinstance(data, pandas.core.frame.DataFrame):
             data = data.values
         self.head = self.build_tree(data)
         
     def predict(self, rows: np.ndarray) -> Any:
+        '''Returns a prediction for a given datapoint'''
         if isinstance(rows, pandas.core.frame.DataFrame):
             rows = rows.values
         if rows.ndim == 1:
