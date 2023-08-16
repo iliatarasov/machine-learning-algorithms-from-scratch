2838b2c955d47dcff3446dd45ad65d3ef2c7485b
iliatarasov <129177692+iliatarasov@users.noreply.github.com>
2023-08-16 18:53:36 +0600

    add docstrings

--- my_models/random_forest.py
+++ my_models/random_forest.py
@@ -4,17 +4,25 @@ import numpy as np
 
 from .desicion_tree import MyDTClassifier
 
-
 class MyRFClassifier:
+    '''Random forest classifier class'''
     def __init__(self, n_estimators: int=50, base_classifier=MyDTClassifier):
+        '''
+        Arguments:
+            n_estimators (int): number of desicion trees in the forest
+            base_classifier (desicion tree model type): base class for a tree
+        '''
         self.n_estimators = n_estimators
         self.base_classifier=base_classifier
     
-    def test_accuracy(self, tree: MyDTClassifier, test_data: np.ndarray) -> float:
+    def test_accuracy(self, tree: MyDTClassifier, 
+                      test_data: np.ndarray) -> float:
+        '''Returns accuracy of a prediction on test_data by a tree'''
         predictions = tree.predict(test_data[:, :-1])
         return sum(predictions == test_data[:, -1]) / len(predictions)
         
     def bootstrap(self, data: np.ndarray) -> tuple[np.ndarray]:
+        '''Partitions given data into bootstrapped and out of bounds parts'''
         choice = np.random.choice(range(len(data)), len(data), 
                                         replace = True)
         bootstrap_idx = np.zeros(data.shape[0], dtype=bool)
@@ -23,6 +31,7 @@ class MyRFClassifier:
         return data[bootstrap_idx], data[test_idx]
     
     def fit(self, data: np.ndarray) -> None:
+        '''Training procedure'''
         self.trees = []
         self.test_scores = []
         for _ in range(self.n_estimators):
@@ -34,6 +43,7 @@ class MyRFClassifier:
             self.test_scores.append(test_score)
         
     def predict(self, data: np.ndarray) -> np.ndarray[Any]:
+        '''Returns a prediction for given data poitns'''
         predictions = []
         for row in data:
             ensemble_predictions = [tree.predict(row) for tree in self.trees]
