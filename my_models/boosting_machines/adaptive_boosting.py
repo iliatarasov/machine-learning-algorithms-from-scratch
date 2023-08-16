d6048d58955131081ec620d84cb5e3cabdd9653c
iliatarasov <129177692+iliatarasov@users.noreply.github.com>
2023-08-16 19:17:55 +0600

    add typing and docstrings

--- my_models/boosting_machines/adaptive_boosting.py
+++ my_models/boosting_machines/adaptive_boosting.py
@@ -1,12 +1,31 @@
 import math
 import numpy as np
 
+from sklearn.tree import DecisionTreeClassifier 
+
 class MyABClassifier():
-    def __init__(self, n_estimators=50, base_classifier=None):
+    '''Adaptive boosting classifier class'''
+    def __init__(self, 
+                 n_estimators: int=50, 
+                 base_clf = DecisionTreeClassifier,
+                 **clf_args) -> None:
+        '''
+        Arguments:
+            n_estimators (int): number of weak estimators
+            base_clf: base classifier class
+            clf_args (optional): collection of keyword arugments for base 
+            classifiers
+        '''
         self.n_estimators = n_estimators
-        self.base_classifier = base_classifier
+        self.base_clf = base_clf
+        self.clf_args = clf_args
         
-    def fit(self, X, y_true):
+    def get_base_classifier(self):
+        '''Returns a new base classifier'''
+        return self.base_clf(**self.clf_args)
+        
+    def fit(self, X: np.ndarray, y_true: np.ndarray) -> None:
+        '''Training procedure'''
         n_samples = len(X)
         # initialize a uniform distribution
         distribution = np.ones(n_samples, dtype=float) / n_samples
@@ -14,7 +33,7 @@ class MyABClassifier():
         self.alphas = []
         for _ in range(self.n_estimators):
             # create a new classifier
-            self.classifiers.append(self.base_classifier())     
+            self.classifiers.append(self.get_base_classifier())     
             self.classifiers[-1].fit(X, y_true, sample_weight=distribution)
             # make a prediction
             y_pred = self.classifiers[-1].predict(X)
@@ -25,7 +44,8 @@ class MyABClassifier():
             distribution = self.update_distribution(y_true, y_pred, 
                                                     distribution, alpha)
             
-    def predict(self, X):
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        '''Returns an array of predictions for given datapoints X'''
         final_predictions = np.zeros(X.shape[0])
         # get weighted votes from each classifier
         for alpha, clf in zip(self.alphas, self.classifiers):
@@ -34,15 +54,22 @@ class MyABClassifier():
         out = np.sign(final_predictions)
         return out
     
-    def get_alpha(self, y, y_pred_t, distribution):
-        # calculate error for each data point
+    def get_alpha(self, 
+                  y: np.ndarray, 
+                  y_pred_t: np.ndarray, 
+                  distribution: np.ndarray) -> float:
+        '''Returns error for a data point'''
         e_i = [int(not y[i] == y_pred_t[i]) for i in range(len(y))]
         e_t = sum([e * w for e, w in zip(e_i, distribution)]) / sum(distribution)
         # return alpha
         return 1/2 * math.log((1-e_t)/e_t)
 
-    def update_distribution(self, y, y_pred_t, distribution, alpha_t):
-        # calculate new distribution and normalize
+    def update_distribution(self, 
+                            y: np.ndarray, 
+                            y_pred_t: np.ndarray, 
+                            distribution: np.ndarray, 
+                            alpha_t: float) -> np.ndarray:
+        '''Updates and normalizes the distribution'''
         new_distribution = np.array([distribution[i] * math.exp(alpha_t * \
             (-1) ** (y[i] == y_pred_t[i])) for i in range(len(y))])
         return new_distribution / new_distribution.sum()
--- my_models/boosting_machines/gradient_boosting.py
+++ my_models/boosting_machines/gradient_boosting.py
@@ -2,19 +2,37 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 
 class MyGBClassifier:
-    def __init__(self, **cfr_args):
+    '''Gradient boosting classifier class'''
+    def __init__(self, 
+                 base_clf=DecisionTreeClassifier,
+                 **cfr_args):
+        '''
+        Arguments:
+            base_clf: base classifier class
+            clf_args (optional): collection of keyword arugments for base 
+            classifiers
+        '''
+        self.base_clf = base_clf
         self.cfr_args = cfr_args
         
-    def get_loss(self, y, y_pred):
+    def get_loss(self, y: np.ndarray, y_pred: np.ndarray) -> float:
+        '''Calculates loss for prediction vector'''
         return 1/2 * np.mean(np.square(y - y_pred))
     
-    def get_gradient(self, y, y_pred):
+    def get_gradient(self, y: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        '''Returns gradient of the loss function at the prediction vector'''
         return - (y - y_pred)
     
     def get_base_classifier(self):
-        return DecisionTreeClassifier(**self.cfr_args)
+        '''Returns a new base classifier'''
+        return self.base_clf(**self.cfr_args)
     
-    def fit(self, X, y, n_estimators=100, learning_rate=0.1):
+    def fit(self, 
+            X: np.ndarray, 
+            y: np.ndarray, 
+            n_estimators: int=100, 
+            learning_rate: float=0.1) -> None:
+        '''Training procedure'''
         self.learning_rate = learning_rate
         self.classifiers = []
         self.losses = []
@@ -34,7 +52,8 @@ class MyGBClassifier:
             y_pred -= learning_rate * residuals
             y_pred = np.sign(y_pred)
         
-    def predict(self, X):
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        '''Returns an array of predictions for given datapoints X'''
         y_pred = np.zeros(len(X))
         for classifier in self.classifiers:
             y_pred -= self.learning_rate * classifier.predict(X)
